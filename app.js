d50c3c421084df5cb52eabe0201920d5bf2f6f91
yididiya <57259174+yididiya-kassahun@users.noreply.github.com>
2023-12-28 15:41:25 +0300

    dall.e 3 integrated

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const TelegramBot = require("node-telegram-bot-api");
+const OpenAIApi = require("openai");
+let fs = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const TOKEN =
-  process.env.TELEGRAM_TOKEN ||
-  "6798703589:AAGchuqNFIhX-I6zlJOcpU9fp_VIg4FhDqI";
+const TOKEN = "6798703589:AAGchuqNFIhX-I6zlJOcpU9fp_VIg4FhDqI";
+
+// *********** OpenAI
+
+const openai = new OpenAIApi({
+  api_key: process.env.OPENAI_API_KEY,
+});
 
 // ************* Bot
 const bot = new TelegramBot(TOKEN, { polling: true });
@@ -18,7 +26,7 @@ function startMenu(msg) {
       one_time_keyboard: true,
     }),
   };
-  bot.sendMessage(msg.chat.id, " start menu ", opts);
+  bot.sendMessage(msg.chat.id, "Image Generator Bot Using DALL.E 3", opts);
 }
 
 bot.onText(/start/, (msg) => {
@@ -37,12 +45,27 @@ bot.onText(/Img Generate/, async (msg) => {
     }
   );
 
-  bot.onReplyToMessage(msg.chat.id, searchInput.message_id, (userSearch) => {
-        bot.sendMessage(
-          msg.chat.id,
-          "got it"
-        );
+  bot.onReplyToMessage(
+    msg.chat.id,
+    searchInput.message_id,
+    async (userSearch) => {
+      //  console.log(userSearch.text);
+      const generatedImage = await openai.images.generate({
+        model: "dall-e-3",
+        prompt: userSearch.text,
+        n: 1,
+        size: "1024x1024",
       });
+
+     let image_url = generatedImage.data[0].url;
+     let revisedPrompt = generatedImage.data[0].revised_prompt;
+     
+      console.log(image_url);
+      console.log(revisedPrompt);
+
+      bot.sendMessage(msg.chat.id, image_url);
+    }
+  );
 });
 
 bot.onText(/developer/, (msg) => {
